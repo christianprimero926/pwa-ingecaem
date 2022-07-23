fb50ac756cb313e187a96a28c63220a774581eca
Christian David Ospina Primero <christianprimero26@hotmail.com>
2022-07-23 03:25:36 -0500

    Ionic/Angular Installation

---
+++ ang_pwa_ingecaem/ang-pwa-ingecaem/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ang_pwa_ingecaem',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
